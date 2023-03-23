fix units

neurax/cell.py
@@ -3,17 +3,21 @@ import jax.numpy as jnp
 
 
 class Cell:
-    def __init__(self, num_branches, parents, nseg_per_branch, length, radius, r_l):
+    def __init__(self, num_branches, parents, nseg_per_branch, length, radius, r_a):
         self.num_branches = num_branches
         self.parents = parents
         self.nseg_per_branch = nseg_per_branch
         self.length_single_compartment = length / nseg_per_branch
         self.radius = radius
-        self.r_l = r_l
+        self.r_a = r_a
 
+        # `radius`: um
+        # `r_a`: ohm cm
+        # `length_single_compartment`: um
         self.coupling_conds = (
-            self.radius / 2.0 / self.r_l / self.length_single_compartment**2
-        )
+            self.radius / 2.0 / self.r_a / self.length_single_compartment**2
+        )  # S * um / cm / um^2 = S / cm / um
+        self.coupling_conds *= (10**7)  # Convert (S / cm / um) -> (mS / cm^2)
         self.num_kids = jnp.asarray(_compute_num_kids(self.parents))
         self.levels = compute_levels(self.parents)
         self.branches_in_each_level = compute_branches_in_level(self.levels)

neurax/integrate.py
@@ -35,7 +35,7 @@ def solve(cell, init, params, stimuli, recordings, t_max, dt: float = 0.025):
     rec_inds = jnp.asarray(rec_inds) * num_states
     stim_inds = [index_of_loc(s.branch_ind, s.loc, cell.nseg_per_branch) for s in stimuli]
     stim_inds = jnp.asarray(stim_inds)
-    stim_currents = jnp.asarray([s.current for s in stimuli])
+    stim_currents = jnp.asarray([s.current for s in stimuli])  # nA
 
     # Save voltage at the beginning.
     saveat = saveat.at[:, 0].set(init[rec_inds])
@@ -77,7 +77,7 @@ def find_root(
     parents,
 ):
 
-    voltages = u[::4]
+    voltages = u[::4]  # mV
     ms = u[1::4]
     hs = u[2::4]
     ns = u[3::4]
@@ -86,9 +86,10 @@ def find_root(
     new_h = solve_gate_exponential(hs, dt, *h_gate(voltages))
     new_n = solve_gate_exponential(ns, dt, *n_gate(voltages))
 
-    na_conds = params[::3] * (ms**3) * hs
-    kd_conds = params[1::3] * ns**4
-    leak_conds = params[2::3]
+    # Multiply with 1000 to convert Siemens to milli Siemens.
+    na_conds = params[::3] * (ms**3) * hs * 1000  # mS/cm^2
+    kd_conds = params[1::3] * ns**4 * 1000 # mS/cm^2
+    leak_conds = params[2::3] * 1000 # mS/cm^2
 
     # External input
     i_ext = get_external_input(

neurax/stimulus.py
@@ -21,6 +21,9 @@ def step_current(
     i_amp: float,
     time_vec: jnp.asarray,
 ):
+    """
+    Return step current in unit nA.
+    """
     zero_vec = jnp.zeros_like(time_vec)
     stim_on = jnp.greater_equal(time_vec, i_delay)
     stim_off = jnp.less_equal(time_vec, i_delay + i_dur)
@@ -36,9 +39,13 @@ def get_external_input(
     length_single_compartment: float,
 ):
     """
-    Compute external input to each compartment.
+    Return external input to each compartment in uA / cm^2.
     """
     zero_vec = jnp.zeros_like(voltages)
-    current = i_stim / 2 / pi / radius / length_single_compartment
+    # `radius`: um
+    # `length_single_compartment`: um
+    # `i_stim`: nA
+    current = i_stim / 2 / pi / radius / length_single_compartment  # nA / um^2
+    current *= 100_000  # Convert (nA / um^2) to (uA / cm^2)
     stim_at_timestep = zero_vec.at[i_inds].set(current)
     return stim_at_timestep