6ee3b1a82188e10db36cd5cddf48163abd303dfd
Michael Deistler <michael.deistler@uni-tuebingen.de>
2023-05-27 09:44:28 +0200

    compartment can be simulated

---
+++ neurax/modules/__init__.py
@@ -0,0 +1,6 @@
+from neurax.modules.channel import Channel
+from neurax.modules.compartment import Compartment
+from neurax.modules.branch import Branch
+from neurax.modules.cell import Cell
+from neurax.modules.network import Network
+from neurax.modules.base import Module
