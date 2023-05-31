c3f5a15085273f662b41a4bf263f8d69b92716db
michaeldeistler <michael.deistler@uni-tuebingen.de>
2023-05-31 16:09:15 +0200

    formatting

--- neurax/utils/cell_utils.py
+++ neurax/utils/cell_utils.py
@@ -25,4 +25,4 @@ def index_of_loc(branch_ind: int, loc: float, nseg_per_branch: int) -> int:
 
 
 def compute_coupling_cond(rad1, rad2, r_a, l1, l2):
-            return rad1 * rad2**2 / r_a / (rad2**2 * l1 + rad1**2 * l2) / l1
+    return rad1 * rad2**2 / r_a / (rad2**2 * l1 + rad1**2 * l2) / l1
