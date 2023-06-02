58600196fe2d64f69b4d9c7dfc03a741cfcd2dca
Michael Deistler <michael.deistler@uni-tuebingen.de>
2023-06-02 17:51:54 +0200

    implement tf

--- neurax/optimize/transforms.py
+++ neurax/optimize/transforms.py
@@ -1,6 +1,5 @@
-from typing import List
+from typing import Dict, List
 import jax.numpy as jnp
-import distrax
 
 
 def sigmoid(x: jnp.ndarray) -> jnp.ndarray:
@@ -14,31 +13,34 @@ def expit(x: jnp.ndarray) -> jnp.ndarray:
 
 
 class MembraneParamTransform:
-    """Membrane parameter transformation utility.
-    It makes the assumption that a particular conductance has the same lower and upper
-    bound in every compartment and in every cell. With this assumption, only the lower
-    and upper bound for every type of conductance have to be provided to this class.
-    """
+    """Membrane parameter transformation utility."""
 
-    def __init__(self, lowers: List[List[float]], uppers: List[List[float]]) -> None:
-        """Build the transformation."""
-        self.lowers = [jnp.stack(vec) for vec in lowers]
-        self.uppers = [jnp.stack(vec) for vec in uppers]
+    def __init__(self, lowers: Dict[str, float], uppers: Dict[str, float]) -> None:
+        """Initialize."""
+        self.lowers = lowers
+        self.uppers = uppers
 
-    def forward(self, params: jnp.ndarray) -> jnp.ndarray:
+    def forward(self, params: List[Dict[str, jnp.ndarray]]) -> jnp.ndarray:
         """Pushes unconstrained parameters through a tf such that they fit the interval."""
         tf_params = []
-        for channel_params, lower, upper in zip(params, self.lowers, self.uppers):
-            transformed = sigmoid(channel_params.T) * (upper - lower) + lower
-            tf_params.append(transformed.T)
+        for param in params:
+            key = list(param.keys())[0]
+            tf = (
+                sigmoid(param[key]) * (self.uppers[key] - self.lowers[key])
+                + self.lowers[key]
+            )
+            tf_params.append({key: tf})
         return tf_params
 
     def inverse(self, params: jnp.ndarray) -> jnp.ndarray:
         """Takes parameters from within the interval and makes them unconstrained."""
         tf_params = []
-        for channel_params, lower, upper in zip(params, self.lowers, self.uppers):
-            transformed = expit((channel_params.T - lower) / (upper - lower))
-            tf_params.append(transformed.T)
+        for param in params:
+            key = list(param.keys())[0]
+            tf = expit(
+                (param[key] - self.lowers[key]) / (self.uppers[key] - self.lowers[key])
+            )
+            tf_params.append({key: tf})
         return tf_params
 
 
