c6830f568519606897b8a9d5dfcae7e67af5888b
coschroeder <cornelius.schroeder@gmail.com>
2023-07-25 14:20:32 +0200

    solver for inf state definition

--- neurax/solver_gate.py
+++ neurax/solver_gate.py
@@ -24,3 +24,16 @@ def solve_gate_exponential(
 
     exp_term = jnp.exp(slope * dt)
     return x * exp_term + xinf * (1.0 - exp_term)
+
+
+def solve_inf_gate_exponential(
+    x: jnp.ndarray,
+    dt: float,
+    s_inf: jnp.ndarray,
+    tau_s: jnp.ndarray,
+):
+    slope = -1.0 / tau_s
+
+    exp_term = jnp.exp(slope * dt)
+
+    return x * exp_term + s_inf * (1.0 - exp_term)
