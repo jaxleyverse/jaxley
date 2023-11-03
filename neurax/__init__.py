92e66e2b079526804f093b38e1e4249e5c3d86f9
Michael Deistler <michael.deistler@uni-tuebingen.de>
2023-11-03 16:38:16 +0100

    bugfixes for recording

--- neurax/__init__.py
+++ neurax/__init__.py
@@ -2,5 +2,4 @@ from neurax.connection import Connection, Connectivity, ConnectivityBuilder
 from neurax.integrate import integrate
 from neurax.modules import *
 from neurax.optimize import ParamTransform
-from neurax.recording import Recording
 from neurax.stimulus import Stimuli, Stimulus, step_current
