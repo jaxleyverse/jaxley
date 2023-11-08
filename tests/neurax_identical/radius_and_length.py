b933eb4675ce6a71a59769db3a8df1e8fec8f69a
Michael Deistler <michael.deistler95@gmail.com>
2023-11-08 10:30:24 +0100

    Add tests for ensuring equal results with older versions

---
+++ tests/neurax_identical/radius_and_length.py
@@ -0,0 +1,14 @@
+def test_radius_length_compartment():
+    pass
+
+
+def test_radius_length_branch():
+    pass
+
+
+def test_radius_length_cell():
+    pass
+
+
+def test_radius_length_net():
+    pass
---
+++ tests/neurax_identical/swc.py
@@ -0,0 +1,6 @@
+def test_swc_cell():
+    pass
+
+
+def test_swc_net():
+    pass
