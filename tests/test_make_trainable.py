6ac6d5273562a4d0e82a8804831aadc0951f57bb
Michael Deistler <michael.deistler95@gmail.com>
2023-12-18 14:02:50 +0100

    Update tests for make_trainable()

--- tests/test_make_trainable.py
+++ tests/test_make_trainable.py
@@ -7,7 +7,7 @@ import jax.numpy as jnp
 import numpy as np
 
 import jaxley as jx
-from jaxley.channels import HH
+from jaxley.channels import HH, Na, K
 from jaxley.synapses import GlutamateSynapse, TestSynapse
 
 
@@ -119,3 +119,146 @@ def test_diverse_synapse_types():
     all_parameters = net.get_all_parameters(params)
     assert np.all(all_parameters["gS"][0] == 2.2)
     assert np.all(all_parameters["gS"][1] == 5.5)
+
+
+
+
+def test_make_all_trainable_corresponds_to_set():
+
+    # Scenario 1.
+    net1, net2 = build_two_networks()
+    net1.insert(HH())
+    params1 = get_params_all_trainable(net1)
+    net2.insert(HH())
+    params2 = get_params_set(net2)
+    assert np.array_equal(params1["HH_gNa"], params2["HH_gNa"], equal_nan=True)
+
+    # Scenario 2.
+    net1, net2 = build_two_networks()
+    net1.cell(1).insert(HH())
+    params1 = get_params_all_trainable(net1)
+    net2.cell(1).insert(HH())
+    params2 = get_params_set(net2)
+    assert np.array_equal(params1["HH_gNa"], params2["HH_gNa"], equal_nan=True)
+
+    # Scenario 3.
+    net1, net2 = build_two_networks()
+    net1.cell(1).branch(0).insert(HH())
+    params1 = get_params_all_trainable(net1)
+    net2.cell(1).branch(0).insert(HH())
+    params2 = get_params_set(net2)
+    assert np.array_equal(params1["HH_gNa"], params2["HH_gNa"], equal_nan=True)
+
+    # Scenario 4.
+    net1, net2 = build_two_networks()
+    net1.cell(1).branch(0).comp(0.4).insert(HH())
+    params1 = get_params_all_trainable(net1)
+    net2.cell(1).branch(0).comp(0.4).insert(HH())
+    params2 = get_params_set(net2)
+    assert np.array_equal(params1["HH_gNa"], params2["HH_gNa"], equal_nan=True)
+    
+
+def test_make_subset_trainable_corresponds_to_set():
+
+    # Scenario 1.
+    net1, net2 = build_two_networks()
+    net1.insert(HH())
+    params1 = get_params_subset_trainable(net1)
+    net2.insert(HH())
+    params2 = get_params_set_subset(net2)
+    assert np.array_equal(params1["HH_gNa"], params2["HH_gNa"], equal_nan=True)
+
+    # Scenario 2.
+    net1, net2 = build_two_networks()
+    net1.cell(0).insert(HH())
+    params1 = get_params_subset_trainable(net1)
+    net2.cell(0).insert(HH())
+    params2 = get_params_set_subset(net2)
+    assert np.array_equal(params1["HH_gNa"], params2["HH_gNa"], equal_nan=True)
+
+    # Scenario 3.
+    net1, net2 = build_two_networks()
+    net1.cell(0).branch(1).insert(HH())
+    params1 = get_params_subset_trainable(net1)
+    net2.cell(0).branch(1).insert(HH())
+    params2 = get_params_set_subset(net2)
+    assert np.array_equal(params1["HH_gNa"], params2["HH_gNa"], equal_nan=True)
+
+    # Scenario 4.
+    net1, net2 = build_two_networks()
+    net1.cell(0).branch(1).comp(0.4).insert(HH())
+    params1 = get_params_subset_trainable(net1)
+    net2.cell(0).branch(1).comp(0.4).insert(HH())
+    params2 = get_params_set_subset(net2)
+    assert np.array_equal(params1["HH_gNa"], params2["HH_gNa"], equal_nan=True)
+    
+
+def build_two_networks():
+    comp = jx.Compartment()
+    branch = jx.Branch(comp, nseg=4)
+    cell = jx.Cell(branch, parents=[-1, 0])
+    net1 = jx.Network([cell, cell])
+    net2 = jx.Network([cell, cell])
+    return net1, net2
+
+
+def get_params_subset_trainable(net):
+    net.cell(0).branch(1).make_trainable("HH_gNa")
+    params = net.get_parameters()
+    params[0]["HH_gNa"] = params[0]["HH_gNa"].at[:].set(0.0)
+    net._to_jax()
+    return net.get_all_parameters(trainable_params=params)
+
+
+def get_params_set_subset(net):
+    net.cell(0).branch(1).set("HH_gNa", 0.0)
+    params = net.get_parameters()
+    net._to_jax()
+    return net.get_all_parameters(trainable_params=params)
+
+def get_params_all_trainable(net):
+    net.cell("all").branch("all").comp("all").make_trainable("HH_gNa")
+    params = net.get_parameters()
+    params[0]["HH_gNa"] = params[0]["HH_gNa"].at[:].set(0.0)
+    net._to_jax()
+    return net.get_all_parameters(trainable_params=params)
+
+def get_params_set(net):
+    net.set("HH_gNa", 0.0)
+    params = net.get_parameters()
+    net._to_jax()
+    return net.get_all_parameters(trainable_params=params)
+
+
+def test_make_trainable_corresponds_to_set_pospischil():
+    """Test whether shared parameters are also set correctly."""
+    net1, net2 = build_two_networks()
+    net1.cell(0).insert(Na())
+    net1.insert(K())
+    net1.cell("all").branch("all").comp("all").make_trainable("vt")
+    params = net1.get_parameters()
+    params[0]["vt"] = params[0]["vt"].at[:].set(0.05)
+    net1._to_jax()
+    params1 = net1.get_all_parameters(trainable_params=params)
+
+    net2.cell(0).insert(Na())
+    net2.insert(K())
+    net2.cell("all").branch("all").comp("all").make_trainable("vt")
+    params = net2.get_parameters()
+    params[0]["vt"] = params[0]["vt"].at[:].set(0.05)
+    net2._to_jax()
+    params2 = net2.get_all_parameters(trainable_params=params)
+    assert np.array_equal(params1["vt"], params2["vt"], equal_nan=True)
+    assert np.array_equal(params1["Na_gNa"], params2["Na_gNa"], equal_nan=True)
+    assert np.array_equal(params1["K_gK"], params2["K_gK"], equal_nan=True)
+
+    # Perform test on simulation.
+    net1.cell(1).branch(1).comp(0.0).record()
+    net2.cell(1).branch(1).comp(0.0).record()
+
+    current = jx.step_current(2.0, 3.0, 0.2, 0.025, 5.0)
+    net1.cell(0).branch(1).comp(0.0).stimulate(current)
+    net2.cell(0).branch(1).comp(0.0).stimulate(current)
+    voltages1 = jx.integrate(net1)
+    voltages2 = jx.integrate(net2)
+    assert np.max(np.abs(voltages1 - voltages2)) < 1e-8
