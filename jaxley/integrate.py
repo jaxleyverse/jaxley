0c60a2898a03b4f988fa025de100eb941f20e3c9
michaeldeistler <michael.deistler95@gmail.com>
2024-01-15 18:52:41 +0100

    Run black and isort

--- jaxley/integrate.py
+++ jaxley/integrate.py
@@ -1,7 +1,8 @@
 from math import prod
-from typing import Dict, List, Optional, Union
+from typing import Dict, List, Optional, Tuple, Union
 
 import jax.numpy as jnp
+import pandas as pd
 
 from jaxley.modules import Module
 from jaxley.utils.jax_utils import nested_checkpoint_scan
@@ -48,11 +49,12 @@ def integrate(
             i_current = module.currents.T
             i_inds = module.current_inds.comp_index.to_numpy()
 
-            # Append stimuli from `data_stimuli`.
-            i_current = jnp.concatenate(
-                [i_current, jnp.expand_dims(data_stimuli[0], axis=0)]
-            )
-            i_inds = np.concatenate([i_inds, data_stimuli[1].comp_index.to_numpy()])
+            if data_stimuli is not None:
+                # Append stimuli from `data_stimuli`.
+                i_current = jnp.concatenate(
+                    [i_current, jnp.expand_dims(data_stimuli[0], axis=0)]
+                )
+                i_inds = np.concatenate([i_inds, data_stimuli[1].comp_index.to_numpy()])
         else:
             i_current = data_stimuli[0]
             i_inds = data_stimuli[1].comp_index.to_numpy()
