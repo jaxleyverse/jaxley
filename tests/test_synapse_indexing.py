5441031b3b3913f28559e473815dbee141800cbc
Kyra Kadhim <kyrakadhim@gmail.com>
2024-02-08 18:31:09 +0100

    Modified synapse testing to include tanh rate synapse

--- tests/test_synapse_indexing.py
+++ tests/test_synapse_indexing.py
@@ -3,15 +3,61 @@ import jax
 jax.config.update("jax_enable_x64", True)
 jax.config.update("jax_platform_name", "cpu")
 
+from typing import List
+
 import numpy as np
+import pytest
 
 import jaxley as jx
 from jaxley.channels import HH
-from jaxley.synapses import GlutamateSynapse, TestSynapse
+from jaxley.synapses import GlutamateSynapse, Synapse, TanhRateSynapse, TestSynapse
 
 
-def test_set_and_querying_params_one_type():
+def test_multiparameter_setting():
+    """
+    Test if the correct parameters are set if one type of synapses is inserted.
+
+    Tests global index dropping: d4daaf019596589b9430219a15f1dda0b1c34d85
+    """
+    comp = jx.Compartment()
+    branch = jx.Branch(comp, nseg=4)
+    cell = jx.Cell(branch, parents=[-1])
+    net = jx.Network([cell for _ in range(2)])
+
+    pre = net.cell(0).branch(0).comp(0.0)
+    post = net.cell(1).branch(0).comp(0.0)
+    pre.connect(post, GlutamateSynapse())
+
+    syn_view = net.GlutamateSynapse
+    syn_params = ["gS", "e_syn"]
+
+    for p in syn_params:
+        syn_view.set(p, 0.32)
+
+
+def _get_synapse_view(net, synapse_name, single_idx=1, double_idxs=[2, 3]):
+    """Access to the synapse view"""
+    if synapse_name == "GlutamateSynapse":
+        full_syn_view = net.GlutamateSynapse
+        single_syn_view = net.GlutamateSynapse(single_idx)
+        double_syn_view = net.GlutamateSynapse(double_idxs)
+    if synapse_name == "TanhRateSynapse":
+        full_syn_view = net.TanhRateSynapse
+        single_syn_view = net.TanhRateSynapse(single_idx)
+        double_syn_view = net.TanhRateSynapse(double_idxs)
+    if synapse_name == "TestSynapse":
+        full_syn_view = net.TestSynapse
+        single_syn_view = net.TestSynapse(single_idx)
+        double_syn_view = net.TestSynapse(double_idxs)
+    return full_syn_view, single_syn_view, double_syn_view
+
+
+@pytest.mark.parametrize(
+    "synapse_type", [GlutamateSynapse, TanhRateSynapse, TestSynapse]
+)
+def test_set_and_querying_params_one_type(synapse_type):
     """Test if the correct parameters are set if one type of synapses is inserted."""
+    synapse_type = synapse_type()
     comp = jx.Compartment()
     branch = jx.Branch(comp, nseg=4)
     cell = jx.Cell(branch, parents=[-1])
@@ -21,69 +67,97 @@ def test_set_and_querying_params_one_type():
         for post_ind in [2, 3]:
             pre = net.cell(pre_ind).branch(0).comp(0.0)
             post = net.cell(post_ind).branch(0).comp(0.0)
-            pre.connect(post, GlutamateSynapse())
+            pre.connect(post, synapse_type)
 
-    net.set("gS", 0.15)
-    assert np.all(net.edges["gS"].to_numpy() == 0.15)
+    # Get the synapse parameters to test setting
+    syn_params = list(synapse_type.synapse_params.keys())
+    for p in syn_params:
+        net.set(p, 0.15)
+        assert np.all(net.edges[p].to_numpy() == 0.15)
 
-    net.GlutamateSynapse.set("gS", 0.32)
-    assert np.all(net.edges["gS"].to_numpy() == 0.32)
+    synapse_name = type(synapse_type).__name__
+    full_syn_view, single_syn_view, double_syn_view = _get_synapse_view(
+        net, synapse_name
+    )
 
-    net.GlutamateSynapse(1).set("gS", 0.18)
-    assert net.edges["gS"].to_numpy()[1] == 0.18
-    assert np.all(net.edges["gS"].to_numpy()[np.asarray([0, 2, 3])] == 0.32)
+    # There shouldn't be too many synapse_params otherwise this will take a long time
+    for p in syn_params:
+        full_syn_view.set(p, 0.32)
+        assert np.all(net.edges[p].to_numpy() == 0.32)
 
-    net.GlutamateSynapse([2, 3]).set("gS", 0.12)
-    assert net.edges["gS"][0] == 0.32
-    assert net.edges["gS"][1] == 0.18
-    assert np.all(net.edges["gS"].to_numpy()[np.asarray([2, 3])] == 0.12)
+        single_syn_view.set(p, 0.18)
+        assert net.edges[p].to_numpy()[1] == 0.18
+        assert np.all(net.edges[p].to_numpy()[np.asarray([0, 2, 3])] == 0.32)
+
+        double_syn_view.set(p, 0.12)
+        assert net.edges[p][0] == 0.32
+        assert net.edges[p][1] == 0.18
+        assert np.all(net.edges[p].to_numpy()[np.asarray([2, 3])] == 0.12)
 
 
-def test_set_and_querying_params_two_types():
+@pytest.mark.parametrize("synapse_type", [TanhRateSynapse, TestSynapse])
+def test_set_and_querying_params_two_types(synapse_type):
     """Test whether the correct parameters are set."""
+    synapse_type = synapse_type()
     comp = jx.Compartment()
     branch = jx.Branch(comp, nseg=4)
     cell = jx.Cell(branch, parents=[-1])
     net = jx.Network([cell for _ in range(4)])
 
     for pre_ind in [0, 1]:
-        for post_ind, synapse in zip([2, 3], [GlutamateSynapse(), TestSynapse()]):
+        for post_ind, synapse in zip([2, 3], [GlutamateSynapse(), synapse_type]):
             pre = net.cell(pre_ind).branch(0).comp(0.0)
             post = net.cell(post_ind).branch(0).comp(0.0)
             pre.connect(post, synapse)
 
-    net.set("gS", 0.15)
-    assert np.all(net.edges["gS"].to_numpy()[[0, 2]] == 0.15)
+    type1_params = list(GlutamateSynapse.synapse_params.keys())
+    synapse_type_params = list(synapse_type.synapse_params.keys())
+
+    default_synapse_type = net.edges[synapse_type_params[0]].to_numpy()[[1, 3]]
+
+    net.set(type1_params[0], 0.15)
+    assert np.all(net.edges[type1_params[0]].to_numpy()[[0, 2]] == 0.15)
+    if synapse_type_params[0] != type1_params[0]:
+        assert np.all(
+            net.edges[synapse_type_params[0]].to_numpy()[[1, 3]] == default_synapse_type
+        )
+    else:
+        default_synapse_type = 0.15
+
+    synapse_type_name = type(synapse_type).__name__
+    synapse_type_full, synapse_type_single, synapse_type_double = _get_synapse_view(
+        net, synapse_type_name, double_idxs=[0, 1]
+    )
+
+    # Generalize to all parameters
+    net.GlutamateSynapse.set(type1_params[0], 0.32)
+    assert np.all(net.edges[type1_params[0]].to_numpy()[[0, 2]] == 0.32)
     assert np.all(
-        net.edges["gC"].to_numpy()[[1, 3]] == 0.5
-    )  # 0.5 is the default value.
+        net.edges[synapse_type_params[0]].to_numpy()[[1, 3]] == default_synapse_type
+    )
 
-    net.GlutamateSynapse.set("gS", 0.32)
-    assert np.all(net.edges["gS"].to_numpy()[[0, 2]] == 0.32)
-    assert np.all(
-        net.edges["gC"].to_numpy()[[1, 3]] == 0.5
-    )  # 0.5 is the default value.
+    synapse_type_full.set(synapse_type_params[0], 0.18)
+    assert np.all(net.edges[type1_params[0]].to_numpy()[[0, 2]] == 0.32)
+    assert np.all(net.edges[synapse_type_params[0]].to_numpy()[[1, 3]] == 0.18)
 
-    net.TestSynapse.set("gC", 0.18)
-    assert np.all(net.edges["gS"].to_numpy()[[0, 2]] == 0.32)
-    assert np.all(net.edges["gC"].to_numpy()[[1, 3]] == 0.18)
+    net.GlutamateSynapse(1).set(type1_params[0], 0.24)
+    assert net.edges[type1_params[0]][0] == 0.32
+    assert net.edges[type1_params[0]][2] == 0.24
+    assert np.all(net.edges[synapse_type_params[0]].to_numpy()[[1, 3]] == 0.18)
 
-    net.GlutamateSynapse(1).set("gS", 0.24)
-    assert net.edges["gS"][0] == 0.32
-    assert net.edges["gS"][2] == 0.24
-    assert np.all(net.edges["gC"].to_numpy()[[1, 3]] == 0.18)
+    net.GlutamateSynapse([0, 1]).set(type1_params[0], 0.27)
+    assert np.all(net.edges[type1_params[0]].to_numpy()[[0, 2]] == 0.27)
+    assert np.all(net.edges[synapse_type_params[0]].to_numpy()[[1, 3]] == 0.18)
 
-    net.GlutamateSynapse([0, 1]).set("gS", 0.27)
-    assert np.all(net.edges["gS"].to_numpy()[[0, 2]] == 0.27)
-    assert np.all(net.edges["gC"].to_numpy()[[1, 3]] == 0.18)
-
-    net.TestSynapse([0, 1]).set("gC", 0.21)
-    assert np.all(net.edges["gS"].to_numpy()[[0, 2]] == 0.27)
-    assert np.all(net.edges["gC"].to_numpy()[[1, 3]] == 0.21)
+    synapse_type_double.set(synapse_type_params[0], 0.21)
+    assert np.all(net.edges[type1_params[0]].to_numpy()[[0, 2]] == 0.27)
+    assert np.all(net.edges[synapse_type_params[0]].to_numpy()[[1, 3]] == 0.21)
 
 
-def test_shuffling_order_of_set():
+@pytest.mark.parametrize("synapse_type", [TanhRateSynapse, TestSynapse])
+def test_shuffling_order_of_set(synapse_type):
     """Test whether the result is the same if the order of synapses is changed."""
+    synapse_type = synapse_type()
     comp = jx.Compartment()
     branch = jx.Branch(comp, nseg=4)
     cell = jx.Cell(branch, parents=[-1])
@@ -98,21 +172,21 @@ def test_shuffling_order_of_set():
         net1.cell(2).branch(0).comp(0.7), GlutamateSynapse()
     )
     net1.cell(2).branch(0).comp(0.4).connect(
-        net1.cell(3).branch(0).comp(0.3), TestSynapse()
+        net1.cell(3).branch(0).comp(0.3), synapse_type
     )
     net1.cell(3).branch(0).comp(0.1).connect(
-        net1.cell(1).branch(0).comp(0.1), TestSynapse()
+        net1.cell(1).branch(0).comp(0.1), synapse_type
     )
 
     # Different order as for `net1`.
     net2.cell(3).branch(0).comp(0.1).connect(
-        net2.cell(1).branch(0).comp(0.1), TestSynapse()
+        net2.cell(1).branch(0).comp(0.1), synapse_type
     )
     net2.cell(1).branch(0).comp(0.6).connect(
         net2.cell(2).branch(0).comp(0.7), GlutamateSynapse()
     )
     net2.cell(2).branch(0).comp(0.4).connect(
-        net2.cell(3).branch(0).comp(0.3), TestSynapse()
+        net2.cell(3).branch(0).comp(0.3), synapse_type
     )
     net2.cell(0).branch(0).comp(1.0).connect(
         net2.cell(1).branch(0).comp(0.1), GlutamateSynapse()
@@ -126,6 +200,6 @@ def test_shuffling_order_of_set():
         net2.cell(i).branch(0).comp(0.0).record()
 
     voltages1 = jx.integrate(net1, t_max=5.0)
-    voltages2 = jx.integrate(net1, t_max=5.0)
+    voltages2 = jx.integrate(net2, t_max=5.0)
 
     assert np.max(np.abs(voltages1 - voltages2)) < 1e-8
