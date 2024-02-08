41377935ddc33c8496cdac50d4cdad0fc3d0903a
Kyra Kadhim <kyrakadhim@gmail.com>
2024-02-08 17:36:06 +0100

    Run isort and black

--- jaxley/synapses/__init__.py
+++ jaxley/synapses/__init__.py
@@ -1,4 +1,4 @@
-from jaxley.synapses.synapse import Synapse
 from jaxley.synapses.glutamate import GlutamateSynapse
+from jaxley.synapses.synapse import Synapse
 from jaxley.synapses.tanh_rate import TanhRateSynapse
 from jaxley.synapses.test import TestSynapse
