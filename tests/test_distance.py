d4b4649b25082519ad382d96e0acd22e8314cf00
jnsbck-uni <jonas.beck@uni-tuebingen.de>
2024-03-18 12:11:53 +0100

    fix: replace comp by loc

--- tests/test_distance.py
+++ tests/test_distance.py
@@ -17,35 +17,35 @@ def test_direct_distance():
     comp = jx.Compartment()
     branch = jx.Branch(comp, nseg=nseg)
     cell = jx.Cell(branch, parents=[-1, 0, 0, 1, 1])
-    cell.branch("all").comp("all").set("length", length)
+    cell.branch("all").loc("all").set("length", length)
     cell.compute_xyz()
-    dist = cell.branch(0).comp(0.0).distance(cell.branch(0).comp(1.0))
+    dist = cell.branch(0).loc(0.0).distance(cell.branch(0).loc(1.0))
     assert dist == (nseg - 1) * length
 
     comp = jx.Compartment()
     branch = jx.Branch(comp, nseg=nseg)
     cell = jx.Cell(branch, parents=[-1, 0, 1])
-    cell.branch("all").comp("all").set("length", length)
+    cell.branch("all").loc("all").set("length", length)
     cell.compute_xyz()
-    dist = cell.branch(0).comp(0.0).distance(cell.branch(2).comp(1.0))
+    dist = cell.branch(0).loc(0.0).distance(cell.branch(2).loc(1.0))
     assert dist == (3 * nseg - 1) * length
 
     move_x = 220.0
     comp = jx.Compartment()
     branch = jx.Branch(comp, nseg=nseg)
     cell = jx.Cell(branch, parents=[-1, 0, 1])
-    cell.branch("all").comp("all").set("length", length)
+    cell.branch("all").loc("all").set("length", length)
     net = jx.Network([cell for _ in range(2)])
     net.compute_xyz()
     net.cell(1).move(move_x, 0, 0)
-    dist = net.cell(1).branch(0).comp(0.0).distance(net.cell(0).branch(0).comp(0.0))
+    dist = net.cell(1).branch(0).loc(0.0).distance(net.cell(0).branch(0).loc(0.0))
     assert dist == move_x
 
-    dist = net.cell(0).branch(0).comp(0.0).distance(net.cell(0).branch(0).comp(0.0))
+    dist = net.cell(0).branch(0).loc(0.0).distance(net.cell(0).branch(0).loc(0.0))
     assert dist == 0.0
 
-    dist = net.cell(1).branch(2).comp(0.3).distance(net.cell(1).branch(2).comp(0.3))
+    dist = net.cell(1).branch(2).loc(0.3).distance(net.cell(1).branch(2).loc(0.3))
     assert dist == 0.0
 
-    dist = net.cell(1).branch(0).comp(0.0).distance(net.cell(1).branch(2).comp(1.0))
+    dist = net.cell(1).branch(0).loc(0.0).distance(net.cell(1).branch(2).loc(1.0))
     assert dist == (3 * nseg - 1) * length
