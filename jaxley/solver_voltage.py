b64f823eb9bbf4031ab0fbf3090252d2230d356b
michaeldeistler <michael.deistler95@gmail.com>
2024-07-16 17:17:45 +0200

    Change license; add license statement

--- jaxley/solver_voltage.py
+++ jaxley/solver_voltage.py
@@ -1,3 +1,6 @@
+# This file is part of Jaxley, a differentiable neuroscience simulator. Jaxley is
+# licensed under the Apache License Version 2.0, see <https://www.apache.org/licenses/>
+
 import jax.numpy as jnp
 from jax import vmap
 from jax.lax import ScatterDimensionNumbers, scatter_add
