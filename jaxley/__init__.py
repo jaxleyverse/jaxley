ded457f3d55cf92e4218eefd842ce32fa8b6651b
jnsbck-uni <jonas.beck@uni-tuebingen.de>
2024-08-30 17:12:35 +0200

    fix: fix circ import

--- jaxley/__init__.py
+++ jaxley/__init__.py
@@ -8,7 +8,6 @@ from jaxley.connect import (
     sparse_connect,
 )
 from jaxley.integrate import integrate
-from jaxley.io import *
 from jaxley.modules import *
 from jaxley.optimize import ParamTransform
 from jaxley.stimulus import datapoint_to_step_currents, step_current
