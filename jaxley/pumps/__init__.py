a3497aef99148d6c6b43678d226c4412bb264d33
michaeldeistler <michael.deistler95@gmail.com>
2024-10-03 15:39:00 +0200

    Allow ion diffusion

---
+++ jaxley/pumps/__init__.py
@@ -0,0 +1,5 @@
+# This file is part of Jaxley, a differentiable neuroscience simulator. Jaxley is
+# licensed under the Apache License Version 2.0, see <https://www.apache.org/licenses/>
+
+from jaxley.pumps.ca_pump import CaPump
+from jaxley.pumps.pump import Pump
