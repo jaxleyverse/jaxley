a2dfe9ba5029a6ea35e37489d78dd1c5e8dbb55c
jnsbck-uni <jonas.beck@uni-tuebingen.de>
2024-11-20 12:47:48 +0100

    fix: isort black

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -2,7 +2,11 @@
 # licensed under the Apache License Version 2.0, see <https://www.apache.org/licenses/>
 
 import os
+import re
+from pathlib import Path
+from typing import List
 
+import numpy as np
 import pytest
 
 
@@ -25,3 +29,32 @@ def test_license(dir):
         assert (
             header == license_txt
         ), f"File {file} does not have the correct license header"
+
+
+def test_rm_all_deprecated_functions():
+    from jaxley.__version__ import __version__ as package_version
+
+    package_version = np.array([int(s) for s in package_version.split(".")])
+
+    decorator_pattern = r"@deprecated(?:_signature)?"
+    version_pattern = r"[v]?(\d+\.\d+\.\d+)"
+
+    package_dir = Path(__file__).parent.parent / "jaxley"
+
+    violations = []
+    for py_file in package_dir.rglob("*.py"):
+        with open(py_file, "r") as f:
+            for line_num, line in enumerate(f, 1):
+                if re.search(decorator_pattern, line):
+                    version_match = re.search(version_pattern, line)
+                    if version_match:
+                        depr_version_str = version_match.group(1)
+                        depr_version = np.array(
+                            [int(s) for s in depr_version_str.split(".")]
+                        )
+                        if not np.all(package_version <= depr_version):
+                            violations.append(f"{py_file}:L{line_num}")
+
+    assert not violations, "\n".join(
+        ["Found deprecated items that should have been removed:", *violations]
+    )
