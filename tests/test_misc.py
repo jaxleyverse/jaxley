a5a1eb9baae7bfb90e105a09cabb952e5094cf65
jnsbck-uni <jonas.beck@uni-tuebingen.de>
2024-12-03 18:56:06 +0100

    enh: add chores workflow as example

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -10,27 +10,6 @@ import numpy as np
 import pytest
 
 
-def list_files(directory):
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if file.endswith(".py"):
-                yield os.path.join(root, file)
-
-
-license_txt = """# This file is part of Jaxley, a differentiable neuroscience simulator. Jaxley is
-# licensed under the Apache License Version 2.0, see <https://www.apache.org/licenses/>"""
-
-
-@pytest.mark.parametrize("dir", ["../jaxley", "."])
-def test_license(dir):
-    for i, file in enumerate(list_files(dir)):
-        with open(file, "r") as f:
-            header = f.read(len(license_txt))
-        assert (
-            header == license_txt
-        ), f"File {file} does not have the correct license header"
-
-
 def test_rm_all_deprecated_functions():
     from jaxley.__version__ import __version__ as package_version
 
