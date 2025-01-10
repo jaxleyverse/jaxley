faadb7079d4f08b227aed4027eb62cea6c9c28e4
jnsbck-uni <jonas.beck@uni-tuebingen.de>
2025-01-10 22:42:42 +0100

    fix: ran isort

--- jaxley/channels/__init__.py
+++ jaxley/channels/__init__.py
@@ -3,5 +3,5 @@
 
 from jaxley.channels.channel import Channel  # isort: skip
 from jaxley.channels.hh import HH
-from jaxley.channels.pospischil import CaL, CaT, K, Km, Leak, Na
 from jaxley.channels.lif import LIF, SmoothLIF
+from jaxley.channels.pospischil import CaL, CaT, K, Km, Leak, Na
