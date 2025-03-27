e0a90943885f2b791347240b53b6c4631f788a86
michaeldeistler <michael.deistler95@gmail.com>
2025-03-27 15:10:05 +0100

    Prepare release 0.8.2

--- jaxley/__version__.py
+++ jaxley/__version__.py
@@ -1,6 +1,6 @@
 # This file is part of Jaxley, a differentiable neuroscience simulator. Jaxley is
 # licensed under the Apache License Version 2.0, see <https://www.apache.org/licenses/>
 
-VERSION = (0, 8, 1)
+VERSION = (0, 8, 2)
 
 __version__ = ".".join(map(str, VERSION))
