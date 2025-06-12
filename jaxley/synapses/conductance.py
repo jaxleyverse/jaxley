ecb7cac280e1a1d41d77381b0f3cf2666dfc4329
Kyra Kadhim <kyrakadhim@gmail.com>
2025-06-12 11:39:25 +0200

    black

--- jaxley/synapses/conductance.py
+++ jaxley/synapses/conductance.py
@@ -10,7 +10,7 @@ from jaxley.synapses.synapse import Synapse
 
 class ConductanceSynapse(Synapse):
     """
-    Compute synaptic current for a simple conductance synapse (no state). This is just 
+    Compute synaptic current for a simple conductance synapse (no state). This is just
     the tanh rate synapse with a conductance current.
     """
 
@@ -19,7 +19,7 @@ class ConductanceSynapse(Synapse):
         prefix = self._name
         self.synapse_params = {
             f"{prefix}_gS": 1e-4,
-            f"{prefix}_e_syn": 0.0, # mV
+            f"{prefix}_e_syn": 0.0,  # mV
             f"{prefix}_x_offset": -70.0,
             f"{prefix}_slope": 1.0,
         }
@@ -41,11 +41,12 @@ class ConductanceSynapse(Synapse):
     ) -> float:
         """Return updated synapse state and current."""
         prefix = self._name
-        tanh_pre_voltage = (
-            -1 *
-            jnp.tanh(
-                (pre_voltage - params[f"{prefix}_x_offset"]) * params[f"{prefix}_slope"]
-            )
+        tanh_pre_voltage = -1 * jnp.tanh(
+            (pre_voltage - params[f"{prefix}_x_offset"]) * params[f"{prefix}_slope"]
         )
-        current = tanh_pre_voltage * params[f"{prefix}_gS"] * (post_voltage - params[f"{prefix}_e_syn"])
+        current = (
+            tanh_pre_voltage
+            * params[f"{prefix}_gS"]
+            * (post_voltage - params[f"{prefix}_e_syn"])
+        )
         return current
