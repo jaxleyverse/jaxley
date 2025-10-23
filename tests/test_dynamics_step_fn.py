e2b992e5ad3b92baf64cb812285d00e757b250c9
Matthijspals <matthijs-pals@hotmail.com>
2025-10-23 10:24:45 +0200

    adjust test

--- tests/test_dynamics_step_fn.py
+++ tests/test_dynamics_step_fn.py
@@ -36,7 +36,7 @@ def test_cycle_consistency(hh_cell):
     cell = hh_cell
     params = []
 
-    states_vec, _, unravel_fn, ravel_fn = build_step_dynamics_fn(
+    states_vec, _, _, unravel_fn, ravel_fn = build_step_dynamics_fn(
         cell, solver="bwd_euler", delta_t=0.025, params=params
     )
 
@@ -50,7 +50,7 @@ def test_jit(hh_cell):
     """Verify that the JIT-compiled step function runs without errors"""
     cell = hh_cell
     params = []
-    states_vec, step_fn, _, _ = build_step_dynamics_fn(
+    states_vec, step_fn, _, _, _ = build_step_dynamics_fn(
         cell, solver="bwd_euler", delta_t=0.025, params=params
     )
 
@@ -110,7 +110,93 @@ def test_jit_and_grad(hh_cell):
         params = transform.forward(opt_params)
 
         # initialise and build the step function
-        states_vec, step_dynamics_fn, _, _ = build_step_dynamics_fn(
+        states_vec, step_dynamics_fn, _, _, _ = build_step_dynamics_fn(
+            cell, solver="bwd_euler", delta_t=0.025, params=params
+        )
+
+        # JIT the step function for speed
+        @jit
+        def step_fn_vec_to_vec(states_vec, externals_now, params=None):
+            states_vec = step_dynamics_fn(
+                states_vec,
+                params,
+                externals=externals_now,
+                external_inds=external_inds,
+                delta_t=0.025,
+            )
+            return states_vec
+
+        states_vecs = [states_vec]
+
+        # Simulate the model
+        for step in range(3):
+            # Get inputs at this time step
+            externals_now = get_externals_now(externals, step)
+            # Step the ODE
+            states_vec = step_fn_vec_to_vec(states_vec, externals_now, params)
+            # Store the state
+            states_vecs.append(states_vec)
+        # Compute the loss at the last time step
+        loss = jnp.mean((states_vecs[-1][state_idx] - target_voltage) ** 2)
+        return loss
+
+    # Compute the gradient of the loss with respect to the parameters
+    grad_loss = value_and_grad(loss, argnums=0)
+    value, gradient = grad_loss(opt_params)
+
+    updates, opt_state = optimizer.update(gradient, opt_state)
+
+    assert np.all(abs(gradient[0]["Leak_gLeak"]) > 0)
+    assert np.all(abs(gradient[1]["v"]) > 0)
+
+
+def test_jit_and_grad_pstate(hh_cell):
+    """Test jitting the dynamics step function and gradients"""
+    cell = hh_cell
+
+    # make some parameters trainable
+    cell.make_trainable("Leak_gLeak")
+    cell.make_trainable("v")
+    params = cell.get_parameters()
+    # Define parameter transform and apply it to the parameters.
+    transform = jx.ParamTransform(
+        [
+            {"Leak_gLeak": jt.SigmoidTransform(0.00001, 0.0002)},
+            {"v": jt.SigmoidTransform(-100, -30)},
+        ]
+    )
+    opt_params = transform.inverse(params)
+    params = transform.forward(opt_params)
+    cell.to_jax()
+
+    # add some inputs
+    externals = cell.externals.copy()
+    external_inds = cell.external_inds.copy()
+    current = jx.step_current(
+        i_delay=1.0, i_dur=2.0, i_amp=0.08, delta_t=0.025, t_max=0.075
+    )
+    data_stimuli = None
+    data_stimuli = cell.branch(0).comp(0).data_stimulate(current, data_stimuli)
+    externals, external_inds = add_stimuli(externals, external_inds, data_stimuli)
+
+    def get_externals_now(externals, step):
+        externals_now = {}
+        for key in externals.keys():
+            externals_now[key] = externals[key][:, step]
+        return externals_now
+
+    target_voltage = -50.0
+    state_idx = -30
+
+    # Define the optimizer
+    optimizer = optax.adam(learning_rate=0.01)
+    opt_state = optimizer.init(opt_params)
+
+    def loss(opt_params):
+        params = transform.forward(opt_params)
+
+        # initialise and build the step function
+        states_vec, step_dynamics_fn, _, _, _ = build_step_dynamics_fn(
             cell, solver="bwd_euler", delta_t=0.025, params=params
         )
 
@@ -164,7 +250,7 @@ def test_build_step_dynamics_fn_branchpoints(branchpoint):
     cell.to_jax()
     params = []
 
-    states_vec, step_dynamics_fn, unravel_fn, _ = build_step_dynamics_fn(
+    states_vec, _, _, unravel_fn, _ = build_step_dynamics_fn(
         cell, solver="bwd_euler", delta_t=0.025, params=params
     )
 
