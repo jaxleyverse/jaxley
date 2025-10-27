5f47792f041a9f292159cc8143bb6599930b1f21
Matthijspals <matthijs-pals@hotmail.com>
2025-10-27 17:10:38 +0100

    all tests running again

--- tests/test_dynamics_step_fn.py
+++ tests/test_dynamics_step_fn.py
@@ -37,10 +37,10 @@ def test_cycle_consistency(hh_cell):
     cell = hh_cell
 
     init_fn, step_fn = build_init_and_step_fn(cell)
-    remove_observables, add_spurious, flatten, unflatten = build_dynamic_state_utils(cell)
+    remove_observables, add_observables, flatten, unflatten = build_dynamic_state_utils(cell)
     all_states, all_params = init_fn([])
     dynamic_states = flatten(remove_observables(all_states))
-    restored = add_spurious(unflatten(dynamic_states), all_params, delta_t=0.025)
+    restored = add_observables(unflatten(dynamic_states), all_params, delta_t=0.025)
     reraveled = flatten(remove_observables(restored))
 
     assert np.allclose(reraveled, dynamic_states)
@@ -49,16 +49,13 @@ def test_cycle_consistency(hh_cell):
 def test_jit(hh_cell):
     """Verify that the JIT-compiled step function runs without errors"""
     cell = hh_cell
-    #states_vec, step_dynamics, _, _, _ = build_dynamic_state_utils(
-    #    cell, solver="bwd_euler", delta_t=0.025
-    #)
-    init_fn, step_fn = build_init_and_step_fn(cell, solver="bwd_euler")
-    remove_observables, add_spurious, flatten, unflatten = build_dynamic_state_utils(cell)
+    init_fn, step_fn = build_init_and_step_fn(cell)
+    remove_observables, add_observables, flatten, unflatten = build_dynamic_state_utils(cell)
     all_states, all_params = init_fn([], None, None)
     dynamic_states = flatten(remove_observables(all_states))
     @jit
     def step_once(dynamic_states):
-        all_states = add_spurious(unflatten(dynamic_states), all_params, 0.025)
+        all_states = add_observables(unflatten(dynamic_states), all_params, 0.025)
         all_states = step_fn(
             all_states, all_params, {}, {}, delta_t=0.025
         )
@@ -112,7 +109,7 @@ def test_jit_and_grad(hh_cell):
     opt_state = optimizer.init(opt_params)
 
     init_fn, step_fn = build_init_and_step_fn(cell)
-    remove_observables, add_spurious, flatten, unflatten = build_dynamic_state_utils(cell)
+    remove_observables, add_observables, flatten, unflatten = build_dynamic_state_utils(cell)
 
     def init_dynamics(params, param_state):
         all_states, all_params = init_fn(params, None, param_state)
@@ -120,7 +117,7 @@ def test_jit_and_grad(hh_cell):
         return dynamic_states, all_params
     @jit
     def step_dynamics(dynamic_states, all_params, externals, external_inds, delta_t=0.025):
-        all_states = add_spurious(unflatten(dynamic_states), all_params, delta_t=delta_t)
+        all_states = add_observables(unflatten(dynamic_states), all_params, delta_t=delta_t)
         all_states = step_fn(
             all_states, all_params, externals, external_inds, delta_t=delta_t
         )
@@ -144,92 +141,6 @@ def test_jit_and_grad(hh_cell):
             dynamic_states_list.append(dynamic_states)
         # Compute the loss at the last time step
         loss = jnp.mean((dynamic_states_list[-1][state_idx] - target_voltage) ** 2)
-        return loss
-
-    # Compute the gradient of the loss with respect to the parameters
-    grad_loss = value_and_grad(loss, argnums=0)
-    value, gradient = grad_loss(opt_params)
-
-    updates, opt_state = optimizer.update(gradient, opt_state)
-
-    assert np.all(abs(gradient[0]["Leak_gLeak"]) > 0)
-    assert np.all(abs(gradient[1]["v"]) > 0)
-
-
-def test_jit_and_grad_pstate(hh_cell):
-    """Test jitting the dynamics step function and gradients"""
-    cell = hh_cell
-
-    # make some parameters trainable
-    cell.make_trainable("Leak_gLeak")
-    cell.make_trainable("v")
-    params = cell.get_parameters()
-    # Define parameter transform and apply it to the parameters.
-    transform = jx.ParamTransform(
-        [
-            {"Leak_gLeak": jt.SigmoidTransform(0.00001, 0.0002)},
-            {"v": jt.SigmoidTransform(-100, -30)},
-        ]
-    )
-    opt_params = transform.inverse(params)
-    params = transform.forward(opt_params)
-    cell.to_jax()
-
-    # add some inputs
-    externals = cell.externals.copy()
-    external_inds = cell.external_inds.copy()
-    current = jx.step_current(
-        i_delay=1.0, i_dur=2.0, i_amp=0.08, delta_t=0.025, t_max=0.075
-    )
-    data_stimuli = None
-    data_stimuli = cell.branch(0).comp(0).data_stimulate(current, data_stimuli)
-    externals, external_inds = add_stimuli(externals, external_inds, data_stimuli)
-
-    def get_externals_now(externals, step):
-        externals_now = {}
-        for key in externals.keys():
-            externals_now[key] = externals[key][:, step]
-        return externals_now
-
-    target_voltage = -50.0
-    state_idx = -30
-
-    # Define the optimizer
-    optimizer = optax.adam(learning_rate=0.01)
-    opt_state = optimizer.init(opt_params)
-
-    def loss(opt_params):
-        params = transform.forward(opt_params)
-
-        # initialise and build the step function
-        states_vec, step_dynamics, _, _, _ = build_step_dynamics_fn(
-            cell, solver="bwd_euler", delta_t=0.025, params=params
-        )
-
-        # JIT the step function for speed
-        @jit
-        def step_fn_vec_to_vec(states_vec, externals_now, params=None):
-            states_vec = step_dynamics(
-                states_vec,
-                params,
-                externals=externals_now,
-                external_inds=external_inds,
-                delta_t=0.025,
-            )
-            return states_vec
-
-        states_vecs = [states_vec]
-
-        # Simulate the model
-        for step in range(3):
-            # Get inputs at this time step
-            externals_now = get_externals_now(externals, step)
-            # Step the ODE
-            states_vec = step_fn_vec_to_vec(states_vec, externals_now, params)
-            # Store the state
-            states_vecs.append(states_vec)
-        # Compute the loss at the last time step
-        loss = jnp.mean((states_vecs[-1][state_idx] - target_voltage) ** 2)
         return loss
 
     # Compute the gradient of the loss with respect to the parameters
@@ -274,53 +185,47 @@ def test_jit_and_grad_pstate(hh_cell):
     optimizer = optax.adam(learning_rate=0.01)
     opt_state = optimizer.init(pstate_values)
 
+
+    init_fn, step_fn = build_init_and_step_fn(cell)
+    remove_observables, add_observables, flatten, unflatten = build_dynamic_state_utils(cell)
+
+    def init_dynamics(params, param_state):
+        all_states, all_params = init_fn(params, None, param_state)
+        dynamic_states = flatten(remove_observables(all_states))
+        return dynamic_states, all_params
+   
+    @jit
+    def step_dynamics(dynamic_states, all_params, externals, external_inds, delta_t=0.025):
+        all_states = add_observables(unflatten(dynamic_states), all_params, delta_t=delta_t)
+        all_states = step_fn(
+            all_states, all_params, externals, external_inds, delta_t=delta_t
+        )
+        dynamic_states = flatten(remove_observables(all_states))
+        return dynamic_states
+
+
     def loss(pstate_values):
 
         # initialise and build the step function
         pstate = cell.data_set("Leak_gLeak", pstate_values[1], None)
         pstate = cell.data_set("v", pstate_values[0], pstate)
-        (
-            states_vec,
-            step_dynamics_fn,
-            _,
-            _,
-            _,
-        ) = build_step_dynamics_fn(
-            cell, solver="bwd_euler", delta_t=0.025, params=params, param_state=pstate
-        )
-
-        # JIT the step function for speed
-        @jit
-        def step_fn_vec_to_vec(
-            states_vec, externals_now, params=None, pstate=pstate_values
-        ):
-            pstate = cell.data_set(
-                "Leak_gLeak", pstate_values[1], None
-            )  # only update leak_gLeak
-            states_vec = step_dynamics_fn(
-                states_vec,
-                params,
-                param_state=pstate,
-                externals=externals_now,
-                external_inds=external_inds,
-                delta_t=0.025,
-            )
-            return states_vec
-
-        states_vecs = [states_vec]
+     
+        dynamic_states, all_params = init_dynamics([], pstate)
+        
+        dynamic_states_list = [dynamic_states]
 
         # Simulate the model
         for step in range(3):
             # Get inputs at this time step
             externals_now = get_externals_now(externals, step)
             # Step the ODE
-            states_vec = step_fn_vec_to_vec(
-                states_vec, externals_now, params, pstate_values
+            dynamic_states = step_dynamics(
+                dynamic_states, all_params, externals_now, external_inds, delta_t=0.025
             )
             # Store the state
-            states_vecs.append(states_vec)
+            dynamic_states_list.append(dynamic_states)
         # Compute the loss at the last time step
-        loss = jnp.mean((states_vecs[-1][state_idx] - target_voltage) ** 2)
+        loss = jnp.mean((dynamic_states_list[-1][state_idx] - target_voltage) ** 2)
         return loss
 
     # Compute the gradient of the loss with respect to the parameters
@@ -341,25 +246,34 @@ def test_build_step_dynamics_fn_branchpoints(branchpoint):
     branch = jx.Branch(comp, 8)
     parents = [-1, 0, 0] if branchpoint else [-1]
     cell = jx.Cell(branch, parents=parents)
-    cell.insert(HH())
+    cell.branch(0).insert(HH())
     cell.insert(Leak())
     cell.to_jax()
 
-    states_vec, _, states_to_pytree, states_to_full_pytree, _ = build_step_dynamics_fn(
-        cell, solver="bwd_euler", delta_t=0.025
-    )
+    # get states and unflatten functions
+    init_fn, step_fn = build_init_and_step_fn(cell)
+    remove_observables, add_observables, flatten, unflatten = build_dynamic_state_utils(cell)
+    all_states, all_params = init_fn([])
+    dynamic_states = flatten(remove_observables(all_states))
 
-    v_len_full = len(states_to_full_pytree(states_vec)["v"])
-    v_len = len(states_to_pytree(states_vec)["v"])
-    i_hh_len = len(states_to_full_pytree(states_vec)["i_HH"])
+    # check lengths
+    tree = unflatten(dynamic_states)
+    full_tree = add_observables(tree, all_params, delta_t=0.025)
+
+    v_len_full = len(full_tree["v"])
+    v_len = len(tree["v"])
+    i_hh_len = len(full_tree["i_HH"])
+    i_hh_m_nonzero = np.count_nonzero(~np.isnan(full_tree["HH_m"]))
 
     if branchpoint:
         assert v_len == 24
         assert v_len_full == 25  # should be n_branches * ncomp_per_branch + 1
         assert i_hh_len == 25
+        assert i_hh_m_nonzero == 9
     else:
         assert v_len == 8
         assert v_len_full == 8
         assert i_hh_len == 8
+        assert i_hh_m_nonzero == 8  
 
 
