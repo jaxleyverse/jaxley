ce96310563086fd7a51447492935317ed64bd4e6
michaeldeistler <michael.deistler95@gmail.com>
2025-12-11 21:14:08 +0100

    unify the synapses

--- jaxley/synapses/__init__.py
+++ jaxley/synapses/__init__.py
@@ -1,6 +1,9 @@
 # This file is part of Jaxley, a differentiable neuroscience simulator. Jaxley is
 # licensed under the Apache License Version 2.0, see <https://www.apache.org/licenses/>
 
+from jaxley.synapses.conductance import ConductanceSynapse
+from jaxley.synapses.current import CurrentSynapse
+from jaxley.synapses.dynamic import DynamicSynapse
 from jaxley.synapses.ionotropic import IonotropicSynapse
 from jaxley.synapses.spike import SpikeSynapse
 from jaxley.synapses.synapse import Synapse
@@ -12,7 +15,11 @@ __all__ = [
     "IonotropicSynapse",
     "SpikeSynapse",
     "Synapse",
+    "DynamicSynapse",
+    "Synapse",
     "TanhConductanceSynapse",
     "TanhRateSynapse",
+    "ConductanceSynapse",
+    "CurrentSynapse",
     "TestSynapse",
 ]
--- jaxley/synapses/ionotropic.py
+++ jaxley/synapses/ionotropic.py
@@ -4,30 +4,57 @@
 from typing import Dict, Optional, Tuple
 
 import jax.numpy as jnp
+<<<<<<< HEAD
 from jax import Array
+=======
+from jax.nn import sigmoid
+>>>>>>> 71c39321 (unify the synapses)
 
-from jaxley.solver_gate import save_exp
+from jaxley.solver_gate import exponential_euler
 from jaxley.synapses.synapse import Synapse
 
 
 class IonotropicSynapse(Synapse):
-    """
-    Compute synaptic current and update synapse state for a generic ionotropic synapse.
+    r"""An ionotropic (state-based) synapse.
+
+    This synapse is similar to the ``DynamicSynapse``, but its time constant is
+    voltage dependent. In addition, this synapse only supports a sigmoidal activation
+    function.
+
+    This synapse implements the following equations:
+
+    .. math::
+
+        I = \overline{g}\, \cdot s\, \cdot (E - V_{\text{post}})
+
+    .. math::
+
+        \tau (V_{\text{pre}}) \, \cdot s = s_{\infty}(V_{\text{pre}}) - s
+
+    .. math::
+
+        s_{\infty}(V_{\text{pre}}) = \sigma\!\left(\frac{V_{\text{pre}} - V_{\text{thr}}}{\Delta}\right)
+
+    .. math::
+
+        \tau(V_{\text{pre}})\, = \frac{1 - s_{\infty}(V_{\text{pre}})}{k_{-}},
 
     The synapse state "s" is the probability that a postsynaptic receptor channel is
     open, and this depends on the amount of neurotransmitter released, which is in turn
-    dependent on the presynaptic voltage.
+    dependent on the presynaptic voltage. This synapse has a time constant which is
+    voltage dependent.
 
     The synaptic parameters are:
-        - gS: the maximal conductance across the postsynaptic membrane (uS)
-        - e_syn: the reversal potential across the postsynaptic membrane (mV)
-        - k_minus: the rate constant of neurotransmitter unbinding from the postsynaptic
-            receptor (s^-1)
+        - ``gS``: the maximal conductance :math:`\overline{g}` (uS).
+        - ``e_syn``: the reversal potential :math:`E` (mV).
+        - ``k_minus``: the rate constant :math:`1/\tau` (:math:`ms^{-1}`).
+        - ``v_th``: the threshold at which the synapse becomes active
+          :math:`V_{\text{thr}}` (mV).
+        - ``delta``: The inverse of the slope of the activation :math:`\Delta` (mV).
 
     Details of this implementation can be found in the following book chapter:
         L. F. Abbott and E. Marder, "Modeling Small Networks," in Methods in Neuronal
         Modeling, C. Koch and I. Sergev, Eds. Cambridge: MIT Press, 1998.
-
     """
 
     def __init__(self, name: Optional[str] = None):
@@ -36,7 +63,7 @@ class IonotropicSynapse(Synapse):
         self.synapse_params = {
             f"{prefix}_gS": 1e-4,  # uS
             f"{prefix}_e_syn": 0.0,  # mV
-            f"{prefix}_k_minus": 0.025,
+            f"{prefix}_k_minus": 0.025,  # 1/ms
             f"{prefix}_v_th": -35.0,  # mV
             f"{prefix}_delta": 10.0,  # mV
         }
@@ -58,12 +85,15 @@ class IonotropicSynapse(Synapse):
         v_th = params[f"{prefix}_v_th"]
         delta = params[f"{prefix}_delta"]
 
-        s_inf = 1.0 / (1.0 + save_exp((v_th - pre_voltage) / delta))
-        tau_s = (1.0 - s_inf) / params[f"{prefix}_k_minus"]
+        s_inf = sigmoid((pre_voltage - v_th) / delta)
+        s_tau = (1.0 - s_inf) / params[f"{prefix}_k_minus"]
 
-        slope = -1.0 / tau_s
-        exp_term = save_exp(slope * delta_t)
-        new_s = states[f"{prefix}_s"] * exp_term + s_inf * (1.0 - exp_term)
+        new_s = exponential_euler(
+            states[f"{prefix}_s"],
+            delta_t,
+            s_inf,
+            s_tau,
+        )
         return {f"{prefix}_s": new_s}
 
     def compute_current(
