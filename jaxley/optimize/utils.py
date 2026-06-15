f71e71ccb10f0f0f22889e747e40eb6c2c409601
iamkzntsv <iamkzntsv.ai@gmail.com>
2026-06-15 16:28:34 +0200

    enh: add uniform distribution to utils (#707)

--- jaxley/optimize/utils.py
+++ jaxley/optimize/utils.py
@@ -1,6 +1,9 @@
 # This file is part of Jaxley, a differentiable neuroscience simulator. Jaxley is
 # licensed under the Apache License Version 2.0, see <https://www.apache.org/licenses/>
 
+from typing import Tuple
+
+import jax
 import jax.numpy as jnp
 from jax import tree_util
 
@@ -9,3 +12,34 @@ def l2_norm(x: "PyTree") -> jnp.array:
     """Return the L2-norm of a pytree. Taken from GH jax/issues/3124."""
     leaves, _ = tree_util.tree_flatten(x)
     return jnp.sqrt(sum([jnp.sum(leaf**2) for leaf in leaves]))
+
+
+class Uniform:
+    """Multidimensional uniform distribution with sample and logprob methods."""
+    def __init__(self, lower: float, upper: float):
+        self.lower = lower
+        self.upper = upper
+
+    def sample(self, key: jnp.ndarray, shape: Tuple[int, ...] = (1,)) -> jnp.ndarray:
+        """Samples from the uniform distribution.
+
+        Args:
+            key: A JAX random key.
+            shape: Sample shape.
+
+        Returns:
+            Samples from the uniform distribution.
+        """
+        return jax.random.uniform(key, shape, minval=self.lower, maxval=self.upper)
+
+    def log_prob(self, x: jnp.ndarray) -> jnp.ndarray:
+        """Computes the log probability of the uniform distribution.
+
+        Args:
+            x: The input to compute the log probability for.
+
+        Returns:
+            The log probability of the uniform distribution.
+        """
+        in_bounds = (x >= self.lower) & (x <= self.upper)
+        return jnp.where(in_bounds, -jnp.log(self.upper - self.lower), -jnp.inf)
